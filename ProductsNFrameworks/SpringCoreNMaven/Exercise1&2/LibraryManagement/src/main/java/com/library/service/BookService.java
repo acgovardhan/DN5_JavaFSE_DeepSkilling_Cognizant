08933938271d8867743ee63bcbc30454d1961b41
Govardhan <mail2acgovardhan@gmail.com>
2026-06-28 13:07:49 +0530

    spring core and maven exercises

---
+++ ProductsNFrameworks/SpringCoreNMaven/Exercise1&2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository repository;
+
+    //setter dependecy injection
+    public void setRepository(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addBook() {
+        System.out.println("BookService: Adding a book...");
+        repository.saveBook();
+    }
+}
